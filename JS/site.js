8a6c66f36e42b5ebbfbcee21b26eb1bd33d27b23
luke-las <84208824+luke-las@users.noreply.github.com>
2022-01-04 03:02:30 -0500

    final product for The Hundred

--- JS/site.js
+++ JS/site.js
@@ -1,4 +1,37 @@
-//call hello
-function helloWorld(){
-    alert("hello world!");
+//get values from interface
+function getValues(){
+    //get values
+    let startValue = Number(document.getElementById("startValue").value);
+    let endValue = Number(document.getElementById("endValue").value);
+
+    //call generateNumbers
+    let numbersGenerated = generateNumbers(startValue, endValue);
+
+    //call displayNumbers
+    displayNumbers(numbersGenerated);
+}
+
+//generate numbers from startValue to endValue
+function generateNumbers(startValue, endValue){
+    let numbersGenerated = [];
+    for (let i = startValue; i <= endValue; i++) {
+        numbersGenerated.push(i);
+    }
+    return numbersGenerated;
+}
+
+//display even numbers in bold
+function displayNumbers(numbersGenerated){
+    let templateRows = "";
+    for(let i = 0; i < numbersGenerated.length; i++){
+        let numberI = numbersGenerated[i];
+        if(numberI%2 == 0){
+            templateRows += `<tr><td style="font-weight:bold">${numberI}</tr></td>`;
+        }
+        else{
+            templateRows += `<tr><td>${numberI}</tr></td>`;
+        }
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
 }
